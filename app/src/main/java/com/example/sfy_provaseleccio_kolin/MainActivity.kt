d87b0f5fd4efab8457fe1a5f596ad01130c4dd85
Francesc Bagué Martí <98266215+FrancescBagueMarti@users.noreply.github.com>
2022-02-14 23:03:49 +0100

    Subida del proyecto

---
+++ app/src/main/java/com/example/sfy_provaseleccio_kolin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sfy_provaseleccio_kolin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
